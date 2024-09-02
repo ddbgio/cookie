ec83a880467e782c948d94568f1d446f15450d45
turk <turk@ddbg.io>
2024-09-02 17:50:50 -0600

    add some validation to basic cookie checks

--- main.go
+++ main.go
@@ -1,10 +1,18 @@
+// TODO give less information to the client when a cookie fails
+
 package main
 
 import (
+	"encoding/base64"
 	"errors"
 	"fmt"
 	"log"
 	"net/http"
+)
+
+var (
+	ErrValueTooLong = errors.New("cookie value too long")
+	ErrInvalidValue = errors.New("invalid cookie value")
 )
 
 func main() {
@@ -19,34 +27,66 @@ func main() {
 	}
 }
 
+// Write a basic insecure cookie
+func Write(w http.ResponseWriter, cookie http.Cookie) error {
+	// only a small subset of US ASCII is supported, so we base64 encode
+	cookie.Value = base64.URLEncoding.EncodeToString([]byte(cookie.Value))
+
+	// not all browsers will prohibit long cookies,
+	// but we set a conservative limit to be sure
+	if len(cookie.String()) > 4096 {
+		return ErrValueTooLong
+	}
+
+	http.SetCookie(w, &cookie)
+	return nil
+}
+
+func Read(r *http.Request, name string) (string, error) {
+	cookie, err := r.Cookie(name)
+	if err != nil {
+		return "", fmt.Errorf("cookie '%s' not found: %w", name, err)
+	}
+	value, err := base64.URLEncoding.DecodeString(cookie.Value)
+	if err != nil {
+		return "", fmt.Errorf("%w: %w", ErrInvalidValue, err)
+	}
+	return string(value), nil
+}
+
 func setCookieHandler(w http.ResponseWriter, r *http.Request) {
 	cookie := http.Cookie{
 		Name:     "example_cookie",
-		Value:    "chocolate_chip",
+		Value:    "peanut_butter",
 		Path:     "/",
 		MaxAge:   86400,
 		HttpOnly: true,
 		Secure:   true,
 		SameSite: http.SameSiteLaxMode,
 	}
+	err := Write(w, cookie)
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "server error", http.StatusInternalServerError)
+		return
+	}
 
-	// adds a `Set-Cookie` header
-	http.SetCookie(w, &cookie)
 	w.Write([]byte("Cookie set!"))
 }
 
 func getCookieHandler(w http.ResponseWriter, r *http.Request) {
-	cookie, err := r.Cookie("example_cookie")
+	value, err := Read(r, "example_cookie")
 	if err != nil {
 		switch {
 		case errors.Is(err, http.ErrNoCookie):
 			http.Error(w, "cookie not found", http.StatusBadRequest)
+		case errors.Is(err, ErrInvalidValue):
+			http.Error(w, "invalid cookie value", http.StatusBadRequest)
 		default:
 			log.Println(err)
 			http.Error(w, "server error", http.StatusInternalServerError)
 		}
 		return
 	}
-
-	fmt.Fprintf(w, "Cookie value is: %s", cookie.Value)
+	w.Write([]byte(value))
 }
